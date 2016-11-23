08d423ac590bb331fbdbf27f676b9fc6b01a01a9
BlackBeard <carvalho.ti.adm@gmail.com>
2016-11-23 12:38:31 -0200

    Calc

--- pratice/calc.cpp
+++ pratice/calc.cpp
@@ -14,8 +14,10 @@ public:
 private:
   bool full{false};
   Token buffer;
-
 };
+
+Token_stream::Token_stream(){}
+
 
 void Token_stream::putback(Token t){
   if(full) error("putback() into a full buffer");
@@ -23,16 +25,38 @@ void Token_stream::putback(Token t){
   full = true;
 }
 
-Token get_token();
-double primary();
-double term();
+Token Token_stream::get(){
+  if(full){
+    full = false;
+    return buffer;
+  }
+  char ch;
+  cin >> ch;
+  switch (ch) {
+    case ';':
+    case 'q':
+    case '(': case ')': case '+': case '-': case '*': case '/':
+      return Token{ch};
+    case '.':
+    case '0': case '1': case '2': case '3': case '4': case '5': case '6':
+    case '7': case '8': case '9':
+    {
+      cin.putback(ch);
+      double val;
+      cin >> val;
+      return Token{'8', val};
+    }
+    default:
+      error("Bad Token");
+  }
+}
+
+
+Token_stream ts;
 double expression();
 
 
-Token get_token(){
-  Token t;
-  return t;
-}
+
 
 double primary(){
   Token t = ts.get();
@@ -69,7 +93,7 @@ double term(){
             break;
       }
       default:
-        ts.putback(d);
+        ts.putback(t);
         return left;
     }
   }
